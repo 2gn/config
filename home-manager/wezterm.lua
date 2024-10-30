b4871915e771cce34a53a73d0b350466fabc9888
Hiram Tanner <101851090+2gn@users.noreply.github.com>
2024-10-30 11:18:05 +0900

    New: wezterm configuration

---
+++ home-manager/wezterm.lua
@@ -0,0 +1,9 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+config.color_scheme = 'flexoki-dark'
+config.font = wezterm.font 'Hasklug Nerd Font'
+
+config.window_decorations = "RESIZE"
+
+return config
